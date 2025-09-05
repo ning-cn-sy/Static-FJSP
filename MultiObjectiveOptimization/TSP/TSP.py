9fc7353eb05c08bc0d9cd0d678bd1d58085734f0
ning-cn-sy <155599207+ning-cn-sy@users.noreply.github.com>
2025-09-05 09:07:15 +0800

    Add files via upload

---
+++ MultiObjectiveOptimization/TSP/TSP.py
@@ -0,0 +1,101 @@
+import time
+
+import numpy as np
+from jmetal.algorithm.multiobjective import NSGAII
+from jmetal.logger import get_logger
+from jmetal.operator import PMXCrossover
+from jmetal.util.termination_criterion import StoppingByEvaluations
+from matplotlib import pyplot as plt
+
+from TSPProblem1 import TSPProblem
+from draw import draw
+# from aaa import aaaA
+from testMutation import testMutation
+
+# 日志,不用管
+logger = get_logger(__name__)
+# 读坐标文件
+data = np.loadtxt(open("zuobiao.csv", "rb"), delimiter=",", skiprows=0, usecols=[0, 1])
+# 定义Problem,要传参,传坐标
+problem = TSPProblem(len(data), zuobiao=data)
+# 一些算法参数
+population_size = 100
+offspring_population_size = 100
+crossover_probability = 1.0
+crossover_distribution_index = 20
+mutation_probability = 1.0 / 2.0
+mutation_distribution_index = 20
+max_evaluations = 300 * 100
+# 定义算法
+algorithm = NSGAII(
+    problem=problem,
+    population_size=population_size,
+    offspring_population_size=offspring_population_size,
+    mutation=testMutation(probability=mutation_probability),
+    crossover=PMXCrossover(probability=crossover_probability),
+    termination_criterion=StoppingByEvaluations(max_evaluations=max_evaluations)
+)
+# algorithm1 = aaaA(
+#     problem=problem,
+#     population_size=population_size,
+#     offspring_population_size=offspring_population_size,
+#     mutation=testMutation(probability=mutation_probability),
+#     crossover=PMXCrossover(probability=crossover_probability),
+#     termination_criterion=StoppingByEvaluations(max_evaluations=max_evaluations)
+# )
+
+# algorithm_old.run()  #原调用方式
+
+# 新调用方式
+"""Execute the algorithm_old."""
+algorithm.start_computing_time = time.time()
+
+logger.debug("Creating initial set of solutions...")
+algorithm.solutions = algorithm.create_initial_solutions()
+
+logger.debug("Evaluating solutions...")
+algorithm.solutions = algorithm.evaluate(algorithm.solutions)
+
+logger.debug("Initializing progress...")
+algorithm.init_progress()
+
+logger.debug("Running main loop until termination criteria is met")
+objective1s = []
+processData = []
+i = 0
+while not algorithm.stopping_condition_is_met():
+    objective1s.append(algorithm.solutions[0].objectives[0])
+    processData.append([i for i in algorithm.solutions[0].variables])
+    print("第%d次", {i})
+    print(processData[len(processData) - 1])
+    print(objective1s[len(objective1s) - 1])
+    algorithm.step()
+    i = i + 1
+    algorithm.update_progress()
+
+logger.debug("Finished!")
+
+algorithm.total_computing_time = time.time() - algorithm.start_computing_time
+solutions = algorithm.get_result()
+
+# from jmetal.lab.visualization import Plot
+
+# plot_front = Plot(title='Pareto front approximation', axis_labels=['x', 'y'])
+# plot_front.plot(solutions, label='aaa', filename='路径规划', format='png')
+
+xs = [i for i in range(len(objective1s))]
+plt.scatter(xs, objective1s)
+plt.show()
+data_collection = []
+
+# 画路线图
+for processDatum in processData:
+    data_point = data
+    data_line = []
+    data_line_part = np.array([[[0, 0], [0, 0]] for i in range(len(processDatum))])
+    for i in range(1, len(processDatum)):
+        data_line_part[i - 1] = [[data[processDatum[i]][0], data[processDatum[i]][1]], [data[processDatum[i - 1]][0],
+                                                                                        data[processDatum[i - 1]][1]]]
+    data_collection.append([data_point, data_line_part])
+print()
+draw(data, data_collection)
---
+++ MultiObjectiveOptimization/TSP/TSPProblem1.py
@@ -0,0 +1,68 @@
+import math
+
+from jmetal.core.problem import PermutationProblem
+from jmetal.core.solution import PermutationSolution, S, CompositeSolution
+
+
+class TSPProblem(PermutationProblem):
+
+    def __init__(self, number_of_variables: int = 30, zuobiao=[]):
+        """:param number_of_variables: Number of decision variables of the problem."""
+        super(TSPProblem, self).__init__()
+        # 定义obj数量，现在只有一个,另一个假设
+        self.obj_directions = [self.MINIMIZE, self.MAXIMIZE]
+        self.obj_labels = ["路径长度", "电量"]
+        # self.lower_bound = number_of_variables * [0.0]
+        # self.upper_bound = number_of_variables * [1.0]
+        self.zuobiao = zuobiao
+
+    def number_of_objectives(self) -> int:
+        return len(self.obj_directions)
+
+    def number_of_constraints(self) -> int:
+        return 0
+
+    def number_of_variables(self) -> int:
+        return len(self.zuobiao)
+
+    def evaluate(self, solution: PermutationProblem) -> PermutationProblem:
+
+        # shunxu = solution.variables[0]
+        shunxu = [yuansu for yuansu in solution.variables]
+        # 这里可以弄成如何计算适应度函数
+        obj1 = 0
+        for i in range(1, len(shunxu)):
+            curCity = shunxu[i]
+            lastCity = shunxu[i - 1]
+            curCityx = self.zuobiao[curCity][0]
+            curCityy = self.zuobiao[curCity][1]
+            lastCityx = self.zuobiao[lastCity][0]
+            lastCityy = self.zuobiao[lastCity][1]
+            obj1 += math.sqrt((curCityx - lastCityx) ** 2 + (curCityy - lastCityy) ** 2)
+
+        # print(shunxu)
+        # print(obj1)
+        solution.objectives[0] = obj1
+        solution.objectives[1] = 0
+
+        return solution
+
+    def create_solution(self) -> S:
+        """Creates a random_search solution to the problem.
+        :return: Solution."""
+        # 可以直接重写Solution
+        new_solution = PermutationSolution(
+            number_of_variables=self.number_of_variables(), number_of_objectives=self.number_of_objectives()
+        )
+        # my_list = [i for i in range(len(self.zuobiao))]
+        # random.shuffle(my_list)
+        # new_solution.variables[0] = my_list
+        for i in range(self.number_of_variables()):
+            new_solution.variables[i] = i
+        # solutionlist = [new_solution,new_solution]
+        # new_solution = PermutationProblem(solutions=solutionlist)
+
+        return new_solution
+
+    def name(self):
+        return "quxinranProblem"
